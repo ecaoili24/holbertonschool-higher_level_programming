26013f3e7c04301624e49b1b97fe8cd9c4f6f5f2
ecaoili24 <erika.caoili@gmail.com>
2019-12-11 03:00:52 +0000

    initial commit of sorted linked list and modified the header file to include the standard lib

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_node - inserts a node into a sorted linked list
+ * @head: the head of the linked list to insert
+ * @number: the number to insert
+ *
+ * Return: the address of the new node or NUll, if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	if (head == NULL)
+		return (NULL);
+
+	newNode->n = number;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (*head);
+	}
+	if (number < current->n)
+	{
+		*head = newNode;
+		newNode->next = current;
+		return (*head);
+	}
+
+	while (current->next->n < number && current->next->next)
+	{
+		current = current->next;
+	}
+	if (current->next->next)
+	{
+		newNode->next = current->next;
+		current->next = newNode;
+	}
+	else
+		current->next->next = newNode;
+
+	return (newNode);
+}
