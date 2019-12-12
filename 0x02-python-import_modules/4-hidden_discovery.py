42b597931076478b5f0765aa3072957bf1c384e3
ecaoili24 <erika.caoili@gmail.com>
2019-12-12 23:24:21 +0000

    initial commit of dir built in

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for names in dir(hidden_4):
+        if not names.startswith("__"):
+            print(names)
+        else:
+            continue
