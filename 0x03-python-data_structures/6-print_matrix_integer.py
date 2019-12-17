80d0a4fdd2b70e7d0595249fcda113f3249d8ac8
ecaoili24 <erika.caoili@gmail.com>
2019-12-17 21:41:36 +0000

    Initial commit of writing a function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in (matrix):
+        for y in x:
+            if x.index(y) - len(x) - 1:
+                print("{:d}".format(y), end=" ")
+            else:
+                print("".format(), end="")
+        print()
