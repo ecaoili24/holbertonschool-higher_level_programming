f12d9fb427ed44bb83556cf4755d13e8e51cfc82
ecaoili24 <erika.caoili@gmail.com>
2020-01-14 22:05:12 +0000

    writing a function that a class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,49 @@
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) *  2
+
+    def __str__(self):
+        s = ""
+        if self.__width > 0 and self.__height > 0:
+            for y in range(self.__height):
+                for x in range(self.__width):
+                    s = s + "#"
+                s = s + "\n"
+        return s[: -1]
+
+    def __repr__(self):
+        return 'Rectangle({}, {})'.format(self.__width, self.__height)
