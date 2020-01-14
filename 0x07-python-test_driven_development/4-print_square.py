a9ad445b2f0f8ef140f1685b7a3ae103abca9a8c
ecaoili24 <erika.caoili@gmail.com>
2020-01-14 08:04:41 +0000

    initial commit of writing a function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module contains the text_indentation function
+"""
+
+def print_square(size):
+    """
+    Result prints a square where size is the len and width
+    of the square
+    """
+    if type(size) is not int or (type(size) is float and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is 0:
+        print("", end="")
+    for i in range(size):
+        for k in range(size):
+            print("#", end="")
+        print()
