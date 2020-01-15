cbe7d6bd231e570358a33f34d69173d4b622e31b
ecaoili24 <erika.caoili@gmail.com>
2020-01-15 23:46:14 +0000

    added the shebang and the correct documentation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle Class"""
+
+
 class Rectangle:
     """Defines a rectangle"""
 
@@ -8,10 +12,17 @@ class Rectangle:
         Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
+    """
+    Initializes a Rectangle instance
 
+    Arguments:
+    width: width of the rectangle
+    height: height of the rectangle
+    """
     @property
     def width(self):
         return self.__width
+    """gets the width of a Rectangle instance"""
 
     @width.setter
     def width(self, value):
@@ -21,9 +32,16 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    """gets the height of a Rectangle instance"""
     @property
     def height(self):
         return self.__height
+
+    """
+    sets the height of Rectangle instance.
+    Argument: value of the height and must be
+    a positive number
+    """
 
     @height.setter
     def height(self, value):
@@ -35,24 +53,45 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
+    """
+    calculates the area of a Rectangle instance
+
+    Returns:Area of the rectangle, given by heart * width
+    """
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
+    """
+    calculates the perimeter of a Rectangle instance
+
+    Returns: Perimeter of the rectangle, given by 2 *
+    (height + width)
+    """
 
     def __str__(self):
         s = ""
         if self.__width > 0 and self.__height > 0:
             for y in range(self.__height):
                 for x in range(self.__width):
-                    s += str(self.print_symbol)
+                    s = str(self.print_symbol)
                 s = s + "\n"
         return s[: -1]
+    """
+    Returns an informal and printable string
+    representation of a Rectangle instance. It is
+    filled with the "#" character
+    """
 
     def __repr__(self):
         return 'Rectangle({}, {})'.format(self.__width, self.__height)
+    """
+    Returns a string representation of a Rectangle instance. It is able
+    to recreate a new instance by using eval()
+    """
 
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances += -1
+    """deletes the rectangle instance"""
