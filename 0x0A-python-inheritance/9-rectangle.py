1132634636a5a4de4bde0677954e542b95938a2a
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 06:50:48 +0000

    initial commit of task 9 and with the parameters: the area() method must be implemented with the print() should print, and str() should return

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module 9-rectangle.
+Creates a Rectangle class.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle with Private instance attributes:
+    - width and height
+    Public method area()
+    Inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """Initializes an instance with Args:
+        - width: size of the rectangle
+        - height: size of the rectangle
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Returns a formatted string"""
+
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """Gets the area of the Rectangle instance.
+        Replaces the area() method from BaseGeometry.
+        """
+
+        return self.__height * self.__width
