26a9948a7d5037ee0c20a9a1e285a0b1ac627ebc
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 06:09:57 +0000

    initial commit of writing a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from.
+Finds an object if it's an instance of a class that is inherited
+--diirectly or indirectly from the specified class."""
+def inherits_from(obj, a_class):
+    return isinstance(obj, a_class) and type(obj) is not a_class
+
+    """Evaluates if an obj is an instance of a class that's
+    inherited from a_class.
+    Args:
+        - obj: object to look at
+        - a_class: class to check
+
+    Returns: True or False
+    """
