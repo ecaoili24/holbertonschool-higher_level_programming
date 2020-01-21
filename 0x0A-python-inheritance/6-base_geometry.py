5bc2322d4b64d814dda5831f6b5a739962e6dcb1
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 06:17:54 +0000

    initial commit of writing a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry.
+Creates a class.
+"""
+
+
+class BaseGeometry:
+    """Class with public instance method"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
+        """Raises an Exception with the following  message
+        """
