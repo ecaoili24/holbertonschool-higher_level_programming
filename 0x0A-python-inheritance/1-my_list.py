d7e21a1a226c30712878802afacd321da6a366c2
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 05:44:42 +0000

    initial commit of writing a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 1-my_list.
+Creates a class MyList that inherits from the list class.
+"""
+
+"""Class MyList inherits from list."""
+class MyList(list):
+    def print_sorted(self):
+        newList = self[:]
+        newList.sort()
+        print("{}".format(newList))
