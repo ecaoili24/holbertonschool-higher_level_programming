09ff42af66041eed2ee482b695211127caeeaa60
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 09:39:52 +0000

    made the file executable

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,12 @@
 """Module 0-lookup.
 Finds a list of available attributes and methods of an object.
 """
+
+
 def lookup(obj):
     return dir(obj)
+
+
 """Returns a list of attributes and methods.
     Args:
         - obj: object to look into
