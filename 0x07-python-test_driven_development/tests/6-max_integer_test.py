e86af7d7dbd9de2490e8b3811a567236c4f000a6
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 08:19:19 +0000

    fixed a syntax error in the def and added a _

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Unittest for max_integer([..]) module
 """
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test.max(self):
+    def test_max(self):
         self.assertEqual(max_integer([2, 4, 6]), 6)
         self.assertEqual(max_integer([4, 2, 1]), 4)
         self.assertEqual(max_integer([1, 10, 8]), 10)
