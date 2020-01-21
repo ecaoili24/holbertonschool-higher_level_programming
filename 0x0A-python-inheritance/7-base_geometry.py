2c3d03ce42aa4e8602353963fc7795916dc05252
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 07:02:16 +0000

    edited to format with pep8 standards

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,15 +5,17 @@ Creates a BaseGeometry class
 
 
 class BaseGeometry:
-     """Class with public instance methods"""
-     def area(self):
+    """Class with public instance methods"""
+
+    def area(self):
         raise Exception("area() is not implemented")
         """Raises an Exception with the following message
         """
-     def integer_validator(self, name, value):
+
+    def integer_validator(self, name, value):
         """Validates value."""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
