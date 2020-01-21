5bdd8122943f2264c4924bc9d1677a35bf73adb8
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 05:03:31 +0000

    initial commit of writing a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 0-lookup.
+Finds a list of available attributes and methods of an object.
+"""
+def lookup(obj):
+    return dir(obj)
+"""Returns a list of attributes and methods.
+    Args:
+        - obj: object to look into
+    """
