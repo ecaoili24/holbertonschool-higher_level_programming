af2f57df77be09226d635537cdd54a17e8dc5ec9
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 06:34:17 +0000

    initial commit of task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module 7-base_geometry
+Creates a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+     """Class with public instance methods"""
+     def area(self):
+        raise Exception("area() is not implemented")
+        """Raises an Exception with the following message
+        """
+     def integer_validator(self, name, value):
+        """Validates value."""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
