fc2df00dee3e194ac5655fe81226d5ab5071104b
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 09:27:08 +0000

    initial commit of writing a function that returns the number of lines of a text file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 1-number_of_lines
+A funtion that counts the  number of lines in a file.
+"""
+
+
+def number_of_lines(filename=""):
+    """Counts lines in filename with Args:
+    - filename: name of the file
+
+    Returns:
+    - number of lines
+    """
+
+    count = 0
+
+    with open(filename,encoding='utf8') as the_file:
+        for lin in the_file:
+            count += 1
+        return count
