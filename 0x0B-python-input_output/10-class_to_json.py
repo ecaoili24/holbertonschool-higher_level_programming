668a7e84a14ceb166c0e7643278edff1b6032489
ecaoili24 <erika.caoili@gmail.com>
2020-01-21 23:17:58 +0000

    initial commit of writing a fucntion that returns the  dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python#
+"""Module 10-class_to_json.py
+A function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean)
+ for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """Creates a dictionary decription of object with Args:
+    - obj: the object to serialize
+    - Return: the dictionary description of object
+    """
+
+    return obj.__dict__
