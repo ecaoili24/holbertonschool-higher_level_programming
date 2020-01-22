937083cbc74d02fca9af8007e30a9a854c684f00
ecaoili24 <erika.caoili@gmail.com>
2020-01-22 00:02:25 +0000

    initial commit of creating a funciton that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module 14-pascal_triangle.py
+A function that returns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """Returns the Pascal Triangle of n with Args:
+    - n: the size of the triangle in rows
+
+    Return: the list of list of int
+    """
+    pascal_triangle = []
+    for x in range(n):
+        row = []
+        for y in range(x + 1):
+            row.append(factorial(x) // (factorial(y) * factorial(x - y)))
+        pascal_triangle.append(row)
+    return pascal_triangle
+
+
+def factorial(num):
+    factorial = 1
+    for x in range(1, num + 1):
+        factorial = factorial * x
+    return factorial
