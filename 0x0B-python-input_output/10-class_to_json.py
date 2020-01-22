5c754551ab6449d75aa9062447c816f301be9cb2
ecaoili24 <erika.caoili@gmail.com>
2020-01-22 02:12:53 +0000

    typo in the path on top

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python#
+#!/usr/bin/python3
 """Module 10-class_to_json.py
 A function that returns the dictionary description with simple data structure
 (list, dictionary, string, integer and boolean)
