01f2bdecb49ef7dab874a8d421fe2e3c5e0b65ee
ecaoili24 <erika.caoili@gmail.com>
2020-01-23 20:15:51 +0000

    added necessary directories for the project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The Base Module for the Base Class
+"""
+
+
+class Base:
+    """class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """__init__ method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
