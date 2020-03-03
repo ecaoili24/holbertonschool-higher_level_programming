ccf1c518934139e2758d1d06989108c93b0ba63d
ecaoili24 <erika.caoili@gmail.com>
2020-03-03 07:50:27 +0000

    Fixed typo in  task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Displays the average temperature (Fahrenheit) by city ordered by
 -- temperature (descending)
 SELECT city, AVG(value) AS "avg_temp" FROM temperatures GROUP BY city ORDER BY
-        avg_temo DESC;
+        avg_temp DESC;
