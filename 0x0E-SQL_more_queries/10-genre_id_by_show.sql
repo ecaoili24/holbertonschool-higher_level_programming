7b17e806bf90d72419cd72ecb984127db8b7280e
ecaoili24 <erika.caoili@gmail.com>
2020-03-03 23:25:45 +0000

    Initial commit of task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least
 -- one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN
+       tv_show_genres
        ON tv_show_genres.show_id = tv_shows.id ORDER BY 1, 2;
