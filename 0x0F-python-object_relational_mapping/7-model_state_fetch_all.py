07cbc7e5b454059a5ceb650f863d24ffc3df073c
ecaoili24 <erika.caoili@gmail.com>
2020-03-30 07:47:32 +0000

    Initial commit of task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+A script that lists all State objects from the database hbtn_0e_6_usa
+'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
