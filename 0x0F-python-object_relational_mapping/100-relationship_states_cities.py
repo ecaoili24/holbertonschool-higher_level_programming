38178c01cc18d7e9678887374be12518391acac1
ecaoili24 <erika.caoili@gmail.com>
2020-03-30 21:18:46 +0000

    Fixed a typo

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,7 +13,7 @@ from sqlalchemy.schema import Table
 
 if __name__ == "__main__":
 
-    eng = create_engine('mysql+mysqldb://' + '{}:{}@localhost/{}'
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
 
