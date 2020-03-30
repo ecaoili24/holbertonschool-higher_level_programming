72399922071273db76fc40d11ddfd7728e74a82a
ecaoili24 <erika.caoili@gmail.com>
2020-03-30 21:17:23 +0000

    Fixed a typo

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -14,6 +14,6 @@ class City(Base):
 
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True,
-                primary_key=True, unique=True nullable=False)
+                primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
