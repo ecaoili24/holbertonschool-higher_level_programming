2daaa2a14e42473fab05fa6db40677895b3bc562
ecaoili24 <erika.caoili@gmail.com>
2020-04-07 20:30:20 +0000

    Modified the comment to fit 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl --silent "$1" | wc -c
+# Script that takes a URL, sends a request to URL, and displays the body size
+curl -s "$1" | wc -c
