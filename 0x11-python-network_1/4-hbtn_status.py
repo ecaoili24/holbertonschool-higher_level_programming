60ad11c2e0102fff27c63f9406e28235822c8a05
ecaoili24 <erika.caoili@gmail.com>
2020-04-13 19:59:45 +0000

    Initial commit of task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    request = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text))
