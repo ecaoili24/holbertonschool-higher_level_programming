1dae5afad37f646bccc414b591fac2487abb1c9e
ecaoili24 <erika.caoili@gmail.com>
2020-04-13 18:19:46 +0000

    Added the guard if name == main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,12 @@
 """Python script that fetches https://intranet.hbtn.io/status"""
 
 import urllib.request
-req = urllib.request.Request('https://intranet.hbtn.iostatus')
-with urllib.request.urlopen(req) as response:
-        page = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(page)))
-        print('\t- content: {}'.format(page))
-        print('\t- utf8 content: {}'.format(page.decode('UTF-8')))
+
+if __name__ == "__main__":
+        req = urllib.request.Request('https://intranet.hbtn.io/status')
+        with urllib.request.urlopen(req) as response:
+                page = response.read()
+                print('Body response:')
+                print('\t- type: {}'.format(type(page)))
+                print('\t- content: {}'.format(page))
+                print('\t- utf8 content: {}'.format(page.decode('UTF-8')))
