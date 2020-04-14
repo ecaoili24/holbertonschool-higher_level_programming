cfc819064cb733f86c02cdb3203db7b5b93af27d
ecaoili24 <erika.caoili@gmail.com>
2020-04-14 01:32:56 +0000

    Fixed a typo

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,6 +4,13 @@ Python script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 
+
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
 import requests
 import sys
 
@@ -15,7 +22,7 @@ if __name__ == "__main__":
     try:
         val = req.json()
         if val == {}:
-            print("No Result")
+            print("No result")
         else:
             print("[{}] {}".format(val.get("id"), val.get("name")))
     except ValueError:
