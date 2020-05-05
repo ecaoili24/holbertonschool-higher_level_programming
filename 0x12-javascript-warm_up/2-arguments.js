3b6ec306dced6f1abcec24cd49ac5dc85cf9550f
ecaoili24 <erika.caoili@gmail.com>
2020-05-05 17:25:28 +0000

    Initial commit of task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Prints a message depending of the # of args passed */
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
