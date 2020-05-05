eaeac63f4929298f168de3cb587a5c71fbaddd95
ecaoili24 <erika.caoili@gmail.com>
2020-05-05 17:54:19 +0000

    Initial commit of task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Prints 3 lines using an array of str and a loop */
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; arr[i]; i++) {
+  console.log(arr[i]);
+}
