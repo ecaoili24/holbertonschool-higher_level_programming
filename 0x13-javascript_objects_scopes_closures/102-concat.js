3ba3018c6fe2fbe6a0a71bf137185d468d8398f1
ecaoili24 <erika.caoili@gmail.com>
2020-05-12 03:53:19 +0000

    fixed spacing error

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// concats two files
+if (process.argv.length !== 5) {
+  console.error('Incorrect num of arguments');
+} else {
+  const fs = require('fs');
+  const targetFile = process.argv[4];
+  let str = '';
+  str = str.concat(fs.readFileSync(process.argv[2]), fs.readFileSync(process.argv[3]));
+  fs.writeFile(targetFile, str, (err) => {
+    if (err) console.log(err);
+  });
+}
