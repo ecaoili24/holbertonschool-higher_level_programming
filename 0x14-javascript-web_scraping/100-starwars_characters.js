d4d3c2b48ef839677df1d00bcdece4057eb1b7f3
ecaoili24 <erika.caoili@gmail.com>
2020-05-13 01:27:37 +0000

    Modified my url variable

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 // Script that prings all characters of a Star Wars movie
 const request = require('request');
-
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
-  function (error, response, body) {
-    if (error) {
-      console.log(error);
-    }
-    for (const role of JSON.parse(body).characters) {
-      request(role, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
-  });
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  for (const role of JSON.parse(body).characters) {
+    request(role, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(JSON.parse(body).name);
+      }
+    });
+  }
+});
