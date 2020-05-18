905781d4a3f621d3cd98a77889316c26fcf7d816
ecaoili24 <erika.caoili@gmail.com>
2020-05-18 21:02:16 +0000

    Added ths status statement for success

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,11 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  for (const movie of data.results) {
-    $('UL#list_movies').append(`<li>$(movie.title}</>`);
-  }
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function
+      (data, status) {
+    if (status === 'success') {
+      const movies = data.results;
+      for (const movies of data.results) {
+        $('UL#list_movies').append(`<li>$(movie.title}</>`);
+      }
+    }
+  });
 });
