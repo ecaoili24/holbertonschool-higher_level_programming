34efcc741917459e8507bd200e1be390d90ec826
ecaoili24 <erika.caoili@gmail.com>
2020-05-18 20:22:08 +0000

    Edited code to fit a second parameter of status

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,8 @@
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data) => {
-  $('DIV#character').append(data.name);
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function
+	(data, status) {
+    if (status === 'success') {
+      $('#character').text(data.name);
+    }
+  });
 });
